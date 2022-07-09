729880f82472c69b8254087bbc1521bc966bd1cd
phillus33 <philipp.roth33@gmail.com>
2022-07-09 20:46:36 +0200

    created first model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+"""Every model represents a table in the DB"""
+from .database import Base, Integer
+from sqlalchemy import Boolean, Column, String
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=True)
