20368d10849ea80dbcffabec9878df2911737179
phillus33 <philipp.roth33@gmail.com>
2022-07-09 20:46:17 +0200

    added docstring to database

--- app/database.py
+++ app/database.py
@@ -1,3 +1,7 @@
+"""
+This allows sqlalchemy to communicate with the db through the engine.
+For further information: https://fastapi.tiangolo.com/tutorial/sql-databases/?h=sql
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
