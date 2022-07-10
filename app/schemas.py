4c25a1a2a52ea3ba46b10f32579d59bff0d74215
phillus33 <philipp.roth33@gmail.com>
2022-07-10 15:33:22 +0200

    added pydantic model for Users

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 # The pydanctic model / schema that defines the structure of a request & response
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+# class Post(BaseModel):
+#     title: str
+#     content: str
+#     published: bool = True
 
 
+# Overall base class
+# Specifies what data the server expects from client
 class PostBase(BaseModel):
     title: str
     content: str
@@ -15,3 +18,27 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+
+# Specifies what data the client will receive
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    # We need this because pydantic expects dict but we pass a sqlalchemy model
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
